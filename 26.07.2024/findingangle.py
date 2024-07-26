5e3403f0e9c019d469134e24b6f067def0e3413e
Kavin744 <147464206+Kavin744@users.noreply.github.com>
2024-07-26 17:58:03 +0530

    Create findingangle.py

---
+++ 26.07.2024/findingangle.py
@@ -0,0 +1,11 @@
+import math
+AB = int(input())
+BC = int(input())
+
+if AB == BC:
+    print (round(math.degrees(math.asin(1/math.sqrt(2)))),chr(176),sep='')
+
+else:
+    x = AB/BC
+    print (round(math.degrees(math.atan(x))),chr(176),sep='')
+    
